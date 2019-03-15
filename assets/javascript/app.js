84275ec4d7f970f5d78c76b9649df5141720b9b4
robertmadril <robertmmadril@gmail.com>
2019-03-14 21:31:08 -0700

    add food api ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,18 @@ $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
   });
+
+var key = "857e05a4f7c6d5050482bf2837e74da2"
+var id = "102f2f2e"
+var queryurl = "https://api.edamam.com/search?q=breakfast&app_id=" + id + "&app_key=" + key + "&from=0&to=3&calories=591-722";
+$.ajax({
+    url: queryurl,
+    method: "GET"
+}).then(function(response){
+;
+    console.log(queryurl)
+    console.log(response);
+
+}, function(err) {
+    console.log("error received:" + err)
+})
