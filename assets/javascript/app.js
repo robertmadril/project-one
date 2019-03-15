0901894576865fc86b0311fa0c4f29431e4a90c6
robertmadril <robertmmadril@gmail.com>
2019-03-14 20:34:14 -0700

    add food ajax call structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,19 @@
 
 Daniel: 
 
-Robert: Create AJAX calls to food API
+Robert:
 Create AJAX calls to exercise API
 Research data collection and output
 */
+var queryurl = ""
+$.ajax({
+    url: queryurl,
+    method: "GET"
+}).then(function(response){
+    var results = response.data;
+
+    console.log(results);
+
+}, function(err) {
+    console.log("error received:" + err)
+})
