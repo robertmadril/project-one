29645eeeb01cb57578fd5dc7ba6a4cec63d3b421
byrdjess <byrdjess@me.com>
2019-03-14 21:22:09 -0700

    carousel-added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,8 @@ Robert: Create AJAX calls to food API
 Create AJAX calls to exercise API
 Research data collection and output
 */
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
