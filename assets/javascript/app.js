416e222dff227c24093aa162648449b8fb743f6a
robertmadril <robertmmadril@gmail.com>
2019-03-14 20:54:23 -0700

    add functioning ajax call with object response in console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,17 @@ Robert:
 Create AJAX calls to exercise API
 Research data collection and output
 */
-var queryurl = ""
+
+var key = "857e05a4f7c6d5050482bf2837e74da2"
+var id = "102f2f2e"
+var queryurl = "https://api.edamam.com/search?q=breakfast&app_id=" + id + "&app_key=" + key + "&from=0&to=3&calories=591-722";
 $.ajax({
     url: queryurl,
     method: "GET"
 }).then(function(response){
-    var results = response.data;
-
-    console.log(results);
+;
+    console.log(queryurl)
+    console.log(response);
 
 }, function(err) {
     console.log("error received:" + err)
