d3c6a76a7ac1b9eed500c95ec5ac69246b43c49b
robertmadril <robertmmadril@gmail.com>
2019-03-23 09:03:36 -0700

    removed ignore status to improve live functionality

---
+++ assets/javascript/config.js
@@ -0,0 +1,13 @@
+    // Initialize Firebase and store api key
+    var firebaseConfig = {
+        apiKey: "AIzaSyAJ7R5RZ30596-0rhCKyrWrUx97oWkGcPA",
+        authDomain: "rmm-salusys.firebaseapp.com",
+        databaseURL: "https://rmm-salusys.firebaseio.com",
+        projectId: "rmm-salusys",
+        storageBucket: "",
+        messagingSenderId: "601851303455"
+    };
+
+    var foodConfig = {
+        foodAPI: "cea6dcb00fmsh4602e43bf21dd0ep15f60fjsn75dd874e804e"
+    }
